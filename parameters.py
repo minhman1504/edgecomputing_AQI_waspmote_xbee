b86c2d87f97a22bde52c0e0fba4c8886caf807ef
minhman1504 <71493623+minhman1504@users.noreply.github.com>
2021-05-04 00:22:06 +0700

    edge_renew
    code đẹp hơn

---
+++ parameters.py
@@ -0,0 +1,18 @@
+#parameters about server
+URL_CLOUD_SERVER = "http://localhost:3000/api"
+
+#parameters about edge
+##Zigbee Protocol
+MAC = ''
+PANID = '1234'
+CHANNEL = 'C'
+
+#Edge Info
+EDGE_ID = '0001'
+EDGE_NAME = 'BinhDuong.DiAn.DongHoa'
+
+#Serial Config
+SERIAL_PORT_URL = 'COM4'
+#SERIAL_PORT_URL = '/dev/ttyUSB0'
+BAUDRATE = 115200
+TIMEOUT = 1
--- script.py
+++ script.py
@@ -1,48 +1,104 @@
 #!/usr/bin/env python3
-print("starting....")
-
+import parameters
 import serial
-import pymongo
+import datetime
 
-myclient = pymongo.MongoClient("mongodb://mongo:27017/test_db")
+#----------------------PARAMATERS--------------------------------------
+portPath = parameters.SERIAL_PORT_URL
+baubrate = parameters.BAUDRATE
+timeout = parameters.TIMEOUT
+EdgeName = parameters.EDGE_NAME + "." + parameters.EDGE_ID
 
-mydb = myclient["test_db"]
-mycol = mydb["rev_data"]
 
 
-# This is the default serial port
-PORT = '/dev/ttyUSB0'
 
-# You may need to further configure settings
-# See the pyserial documentation for more info
-# https://pythonhosted.org/pyserial/pyserial_api.html#classes
-ser = serial.Serial(port=PORT,baudrate=115200,timeout=1)
+#---------------------FUNCTIONS-----------------------------------------
 
-try:
-    while True:
-        # Read raw data from the stream
-        # Convert the binary string to a normal string
-        # Remove the trailing newline character
-        
-        message = ser.readline().decode('utf8','ignore')
-        print(message)
-        print(str(message))
+#return a serial obj if fail return -1
+def create_serial_obj():
+    try:
+        serial_listen = serial.Serial(portPath, baubrate, timeout = timeout)
+        return serial_listen
+    except Exception as e:
+        print("Create Serial Obj Error !")
+        print(e)
+        return -1
 
-        if len(message) != 0:
-            record = message.split('#')
-            record = list(filter(None, record))
-            print(record)
 
-            db_record = {
-            'flield_00' : record[0],
-            'flield_01' : record[1],
-            'Node_Name' : record[2],
-            'flield_03' : record[3],
-            'Sensor_01' : record[4],
-            'Sensor_02' : record[5]
-            }
 
-finally:
-    ser.close()
+#return a index if fail return -1
+''' data frame format
+
+Data Waspmote & Arduino to Edge
+# <> # <> # DeviceID # <> # DeviceType # TypesOfData # <data01> # <data02> # <data...> # <>
+  0    1     2          3    4             5             6           7        ...    
+
+  record[2]: DeviceID
+  record[4]: DeviceType
+  record[5]: TypesOfData
+  record[6] -> end : data
+''' 
+def handle_message(mess):
+    mess = mess.decode('utf8','ignore')
+    record = mess.split('#')
+    record = list(filter(None, record))
+    if len(record) != 0:   
+        DeviceID = record[2]
+        DeviceType = record[4].replace('STR:', '')
+        TypesOfData = record[5].replace('STR:', '').split('?')
+
+        framedata = {}
+
+        framedata['DeviceName'] = EdgeName + '_' + DeviceType + '_' + DeviceID
+
+        data = []
+        k=6
+        for i in TypesOfData:
+            obj = {}
+            obj[i] = record[k]
+            k = k + 1
+            data.append(obj)
+
+        framedata['Data'] = data
+
+        framedata['Time'] = datetime.datetime.now().strftime("%c")
+
+        return framedata
+
+    else:
+        return -1
+
+#store to file .csv or send to database
+def store_index(index):
+    ## temp to print data to console
+    print(index)
+
 
     
+
+def read_serial_data(serial_listen):
+    #do somethings
+    try:
+        serial_listen.flushInput()
+        while True:
+            raw_message = serial_listen.readline()
+            index = handle_message(raw_message)
+            if index != -1:
+                store_index(index)
+
+    except Exception as e:
+        print("Read serial error !")
+        print(e)
+
+    finally:
+        serial_listen.close()
+
+
+
+# --------------------------------RUNNING CODE-------------------------------
+print("starting....")
+serial_listen = create_serial_obj()
+
+if serial_listen != -1:
+    read_serial_data(serial_listen)
+
